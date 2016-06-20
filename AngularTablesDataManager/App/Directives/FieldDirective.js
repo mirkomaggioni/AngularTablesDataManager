8646e294549015275bf830ed872cb74d0cf0835b
Mirko Maggioni <mirko.maggioni@gmail.com>
2016-06-20 19:17:52 +0200

    Adding FieldDirective views

--- AngularTablesDataManager/App/Directives/FieldDirective.js
+++ AngularTablesDataManager/App/Directives/FieldDirective.js
@@ -7,30 +7,44 @@ var AngularTablesDataManagerApp;
         var FieldDirective = (function () {
             function FieldDirective(fieldConfigurationService) {
                 var _this = this;
-                this.restrict = 'A';
+                this.restrict = 'E';
                 this.scope = {
                     entityName: '=',
-                    tipology: '='
+                    tipology: '=',
+                    property: '='
                 };
+                this.template = '<ng-include src="contentUrl" />';
                 this.link = function (scope, element, attrs) {
                     _this.fieldConfigurationsService.getFieldConfigurationTipology(scope.entityName, scope.tipology.Name).then(function (data) {
-                        var tipology = data;
-                        if (tipology == '') {
-                            tipology = scope.tipology.Type;
+                        var tipology = scope.tipology.Type;
+                        if (data != "") {
+                            tipology = data;
+                            _this.fieldConfigurationsService.getFieldValues(scope.entityName, scope.tipology.Name).then(function (data) {
+                                scope.values = data;
+                                _this.openView(scope, tipology);
+                            });
                         }
-                        if (tipology == 'Edm.String') {
-                            element.attr('type', 'text');
-                        }
-                        else if (tipology.toLowerCase().indexOf('radio') != -1) {
-                            element.attr('type', 'radio');
-                        }
-                        else if (tipology.toLowerCase().indexOf('int') != -1) {
-                            element.attr('type', 'number');
+                        else {
+                            _this.openView(scope, tipology);
                         }
                     });
                 };
                 this.fieldConfigurationsService = fieldConfigurationService;
             }
+            FieldDirective.prototype.openView = function (scope, tipology) {
+                if (tipology.toLowerCase().indexOf('int') != -1) {
+                    scope.contentUrl = 'app/directives/InputNumber.html';
+                }
+                else if (tipology.toLowerCase().indexOf('radio') != -1) {
+                    scope.contentUrl = 'app/directives/InputRadio.html';
+                }
+                else if (tipology.toLowerCase().indexOf('select') != -1) {
+                    scope.contentUrl = 'app/directives/InputSelect.html';
+                }
+                else {
+                    scope.contentUrl = 'app/directives/InputText.html';
+                }
+            };
             return FieldDirective;
         }());
         Directives.FieldDirective = FieldDirective;
--- AngularTablesDataManager/App/Directives/FieldDirective.ts
+++ AngularTablesDataManager/App/Directives/FieldDirective.ts
@@ -7,38 +7,54 @@ module AngularTablesDataManagerApp.Directives {
 
     interface IFieldDirectiveScope extends ng.IScope {
         entityName: string;
+        contentUrl: string;
         tipology: models.MetadataProperty;
+        property: models.RowProperty;
+        values: Array<string>;
     }
 
     export class FieldDirective implements ng.IDirective {
         fieldConfigurationsService: services.FieldConfigurationsService;
-        public restrict = 'A';
+        public restrict = 'E';
         public scope = {
             entityName: '=',
-            tipology: '='
+            tipology: '=',
+            property: '='
         };
+        public template = '<ng-include src="contentUrl" />';
         public link = (scope: IFieldDirectiveScope, element: JQuery, attrs: IArguments) => {
-
-            this.fieldConfigurationsService.getFieldConfigurationTipology(scope.entityName, scope.tipology.Name).then((data) => {
-                var tipology: string = data;
-                if (tipology == '') {
-                    tipology = scope.tipology.Type;
+            this.fieldConfigurationsService.getFieldConfigurationTipology(scope.entityName, scope.tipology.Name).then((data: string) => {
+                var tipology: string = scope.tipology.Type;
+                if (data != "") {
+                    tipology = data;
+                    this.fieldConfigurationsService.getFieldValues(scope.entityName, scope.tipology.Name).then((data: Array<string>) => {
+                        scope.values = data;
+                        this.openView(scope, tipology);
+                    });
                 }
-
-                if (tipology == 'Edm.String') {
-                    element.attr('type', 'text');
-                }
-                else if (tipology.toLowerCase().indexOf('radio') != -1) {
-                    element.attr('type', 'radio');
-                }
-                else if (tipology.toLowerCase().indexOf('int') != -1) {
-                    element.attr('type', 'number');
+                else {
+                    this.openView(scope, tipology);
                 }
             });
         }
 
         public constructor(fieldConfigurationService: services.FieldConfigurationsService) {
             this.fieldConfigurationsService = fieldConfigurationService;
+        }
+
+        private openView(scope: IFieldDirectiveScope, tipology: string) {
+            if (tipology.toLowerCase().indexOf('int') != -1) {
+                scope.contentUrl = 'app/directives/InputNumber.html';
+            }
+            else if (tipology.toLowerCase().indexOf('radio') != -1) {
+                scope.contentUrl = 'app/directives/InputRadio.html'
+            }
+            else if (tipology.toLowerCase().indexOf('select') != -1) {
+                scope.contentUrl = 'app/directives/InputSelect.html';
+            }
+            else {
+                scope.contentUrl = 'app/directives/InputText.html';
+            }
         }
     }
 
